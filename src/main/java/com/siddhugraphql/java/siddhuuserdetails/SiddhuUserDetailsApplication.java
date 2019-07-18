136bf4f2c528f68236923a435a999f2fc9221b9f
Siddharatha.H.Dhumale <shdhumale@gmail.com>
2019-07-18 11:55:32 +0530

    Commit to master

---
+++ src/main/java/com/siddhugraphql/java/siddhuuserdetails/SiddhuUserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.siddhugraphql.java.siddhuuserdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiddhuUserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiddhuUserDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddhugraphql/java/siddhuuserdetails/SiddhuUserDetailsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siddhugraphql.java.siddhuuserdetails;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiddhuUserDetailsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
